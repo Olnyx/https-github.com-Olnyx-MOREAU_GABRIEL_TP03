cf6d11c535cabc06a75382aa692c39597d8f3f96
Gabriel <bob@hotmail.com>
2021-04-14 17:22:57 -0400

    Début de cration du tank.
    Model du tank
    Mouvement du tank
    Particules du tire
    Début des audio.

---
+++ Assets/Sherman/Script/PlayerFire.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    [Header("Particle")]
+    public ParticleSystem particle_1;
+    public ParticleSystem particle_2;
+
+    [Header("Sound effects")]
+    public AudioSource gun_firing;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Fire!");
+            gun_fire();
+        }
+    }
+
+    void gun_fire()
+    {
+        particle_1.Play();
+        particle_2.Play();
+    }
+}
